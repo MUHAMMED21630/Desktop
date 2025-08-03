b040c502944b05100295d2ab6c69f955fdbe3635
MUHAMMED <muhammedisik208@gmail.com>
2025-08-04 00:15:21 +0300

    python da ilk çallışmalar

---
+++ python/değişkenler.py
@@ -0,0 +1,17 @@
+x = 3
+print (x)
+
+isim = "muhammed" 
+fiyat = 13.95 
+aldı = True
+sayfa = 195
+x = 3 
+
+isim = "Moby Dick" 
+agırlık= 13.45 
+sayfa = 195 
+yeni = True 
+
+print(isim)
+print(agırlık)
+print(sayfa)
