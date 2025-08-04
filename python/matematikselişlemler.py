30080966358df3faa30392b98897a273a8cad55a
MUHAMMED <muhammedisik208@gmail.com>
2025-08-05 00:06:03 +0300

    python

---
+++ python/matematikselişlemler.py
@@ -0,0 +1,43 @@
+# x=8
+# y=4
+
+# print(x+y)
+# print(x-y)
+# print(y*x)
+# print(y/x)
+
+# print(x//y) # // tam Bölme
+# print(x%y) # % kalan
+# print(x**y) # ** üssü alma
+# print(x==y) # == eşit mi
+# print(x!=y) # != eşit değil mi
+# print(x>y) # > büyük mü
+# print(x<y) # < küçük mü
+# print(x>=y) # >= büyük eşit mi
+# print(x<=y) # <#= küçük eşit mi
+
+# x=14
+# y=12
+
+
+
+
+
+# x = input("lütfen birinci sayıyı giriniz: ")
+# y = input("lütfen ikinci sayıyı giriniz: ")
+# print(int(x) + int(y))
+
+
+print(round(3.14159)) # round() fonksiyonu sayıyı en yakın tam sayıya yuvarlar
+print(abs(-8)) # abs() fonksiyonu sayının mutlak değerini alır
+print(pow(4, 8)) # pow() fonksiyonu sayının üssünü alır
+print(max(12, 14, 8, 4)) # max() fonksiyonu verilen sayılardan en büyüğünü döner
+print(min(12, 14, 8, 4)) # min() fonksiyonu verilen sayılardan en küçüğünü döner
+print(sum([12, 14, 8, 4])) # sum() fonksiyonu verilen sayıların toplamını döner
+print(divmod(12, 14)) # divmod() fonksiyonu verilen iki sayının bölümünü ve kalanı döner
+print(bin(12)) # bin() fonksiyonu sayıyı ikilik tabanda gösterir
+
+import math # math modülü matamatik işlemler için kullanılır
+print(math.sqrt(14)) # math.sqrt() fonksiyonu sayının karekökünü alır
+print(math.factorial(4)) # math.factorial() fonksiyonu sayının faktöriyelini alır
+print(math.pi) # math.pi sayısı pi sayısını verir
---
+++ python/türdegiştirme.py
@@ -0,0 +1,8 @@
+isim = "muhammed"
+yaş = 19
+print ("isim" + " " + str(yaş) + " " + "yaşında" )
+
+int()
+float()
+str()
+bool()
