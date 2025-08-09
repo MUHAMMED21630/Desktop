b34c590b823e32f9fbbcadf9f174d3b551d3a0cb
MUHAMMED <muhammedisik208@gmail.com>
2025-08-09 23:50:53 +0300

    başarılı

--- python/değişkenler.py
+++ python/değişkenler.py
@@ -23,3 +23,16 @@ yeni = True
 # Python’da string, çift tırnak 
 # ("merhaba") veya tek tırnak 
 # ('merhaba') ile yazılır.
+
+# print(type(isim)) -> bu yapının hangi veri tipinde olduğunu gösterir.
+# turtle.donne()-> turtle done() komutu, turtle grafiklerinin tamlandığını ve pencerenin açık kalmasını sağlar.
+# turtle.end_fill()turtle.end_fill() komutu, turtle grafiklerinde bir şeklin doldurulmasını tamamlar. bu komut, genellikle bir şekil çizildikten sonra kullanılır ve o şeklin içini doldurmak için kullanılır.
+# arrow.penup()arrow.penup() komutu, turtle grafiklerinde ok işaretinin kalemini kaldırır. Bu, ok işaretinin hareket ederken çizgi bırakmamasını sağlar.
+# anchor = anchor() fonksiyonu, turtle grafiklerinde bir nesnenin referans noktasını ayarlar. bu, nesnenin konumunu ve yönünü belirlemek için kullanılır.
+# aiter()= aiter komutu, turtle grafiklerinde bir nesnenin hatlarını çizer. bu komut, genellikle bir şekil çizildikten sonra kullanılır ve o şeklin kenarlarını çizmek için kullanılır.
+# arow.goto()arow.goto() komutu, turtle grafiklerinde ok işaretini belirli bir konuma taşır. bu komut, ok işaretinin hareket etmesini sağlar ve genellikle bir şekil çizildikten sonra kullanılır.
+# ExceptionGroup = exceptionGroup, python'da birden fazla istisna grubunu temsil eden bir sınıftır. bu sınıf, birden fazla istisna türünü tek bir grup altında toplamak için kullanılır ve bu gruptaki tüm istisnaları aynı anda işlemek için kullanılabilir.
+# ConnectionAbortedError = ConnectionAbortedError , python'da bir bağlantının beklenmedik bir şekilde kesildiğini gösteren bir istisna türüdür. bu istisna, genellikle bir ağ bağlantısı veya veritabanı bağlantısı gibi iletşim hataları sırasında ortaya çıkar ve programın bu hatayı ele almasını sağlar.
+# StopAsyncIteration  
+# StopAsyncIteration = StopAsyncIteration, python'da asenkron bir iteratörün sonuna ulaşıldığında ortaya çıkan bir istisna türüdür. bu istisna, asenkron iteratörlerin döngülerde kullanılmasını sağlar ve iteratörün daha fazla öğe üretmediğini belirtir.
+# NotADirectoryError = NotADirectroryError, python'da bir dosya veya dizin beklenirken, ancak belirtilen yolun bir dizin olmadığı durumlarda ortaya çıkan bir istisna türüdür. bu istisna, genellikle dosya işlemleri sırasında yanlış bir yol belirtildiğinde ortaya çıkar ve  proogramın bu hatayı ele almasnı sağlar.
--- python/input.py
+++ python/input.py
@@ -4,3 +4,7 @@
 # sever = input("ne yemeyi seversiniz? ")
 # print("ben de " + sever + " yemeyi seviyorum")
 # print("merhaba " + isim + " " + sever + " yemeyi seviyor")
+
+# input("") fonksiyonu kullanıcıdan veri almak için kullanılır.
+# input() fonksiyonunu kullanarak kulanıcıdan isim,yaş,ilgi alanı gibi veriler alabiliriz.
+# inpput fonksiyonu her zaman string veri tipi döndürür.
