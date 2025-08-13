d281d8f8586155b181dd7ea90bf43f97b0848d6c
MUHAMMED <muhammedisik208@gmail.com>
2025-08-13 18:10:38 +0300

    devam

---
+++ python/string_ile_fonksiyonlar.py
@@ -0,0 +1,28 @@
+isim = 'muhammed yazılım ile uğraşmıyor'
+#       # 01234567    
+# print(isim[-4:-1])
+
+#  metinin uzunluğunu bulma bunun için len() fonksiyonu kullanılır
+# print(len(isim))
+#  metinlerdeki karakterleri değiştirmek için replace() fonksiyonu kullanılır
+# print(isim.replace('uğraşmıyor', 'uğraşıyor [değiştirildi]')) 
+#  unlu = isim.upper() metini büyük harfe çevirir
+#  print(isim.upper())
+#  to_lower = isim.lower() metini küçük harflere çevirir
+# print(isim.lower())
+#  title = isim.title() metini baş harfleri büyük yapar
+# print(isim.title())
+#  strip() metini başındaki ve sonundaki boşlukları siler
+#  print(isim.strip())
+#  harf bulmak için find() fonksiyonu kullanılır
+#  print(isim.find('m'))
+#  metindeki karakterlerin sayısını bulmak için count() fonksiyonu kullanılır
+# print(isim.count('a'))
+# not karakter neyse o yazılmalı çünkü python büyük küçük harfe duyarlıdır
+# split() metini parçalara ayırır
+# print(isim.split(' ')) boşluk karakterine göre ayırır
+# join() metini birleştirir
+#  print(' '.join(['merhaba', 'muhammed'])) boşluk karakteri ile birleştirir
+# print(''.join(['merhaba', 'muhammed']))
+# print(isim[::-1]) metini tersine çevirir
+# capitalize = caiptalize işe yarar karakterleri düzenler 
---
+++ python/string_proje.py
@@ -0,0 +1,6 @@
+isim = input("isminizi girin:")
+
+
+duzenlenmis_isim = isim.capitalize()
+
+print("düzenlenmiş isim" , duzenlenmis_isim)
